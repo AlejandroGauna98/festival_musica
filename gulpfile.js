15becb47ef560f367ac7a9903345749c15688d7a
Alejandro <aleepedro7@gmail.com>
2022-05-02 17:46:16 -0300

    para clonar

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
-const { src, dest } =  require('gulp');
+const { src, dest, watch } =  require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done){
-    src('src/scss/app.scss').pipe(sass()).pipe(dest('build/css'));
-    
+    src('src/scss/**/*.scss').pipe(plumber()).pipe(sass()).pipe(dest('build/css'));//con 'src' identificamos el archivo a compilar, el primer pipe lo compila y
+                                                                    // el segundo pipe lo almacena en el disco duro
 
     done();
 }
 
+function dev(done){
+    watch('src/scss/**/*.scss',css);
+    done();
+}
+
 exports.css = css;
+exports.dev = dev;
