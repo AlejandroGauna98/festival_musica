230653bd8514a43fed7bc847c80c5d3691b2fd3d
Alex <tuemail@gmail.com>
2022-05-14 21:05:24 -0300

    no anda galeria

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMcontentLoaded', function(){
+    iniciarApp();
+});
+
+function iniciarApp(){
+    crearGaleria();
+}
+
+function crearGaleria(){
+    const galeria = document.querySelector('.galeria-imagenes');
+    
+    for(let i = 1; i <= 12; i++){
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/${i}.avif" type="image/avif">
+            <source srcset="build/img/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="img/${i}.jpg" alt="Imagen Galeria">`;
+
+        galeria.appendChild(imagen);
+    }
+}
+
